a928b0ca7fd259cc522ff2d4a92c6de8d76cb137
W. Ryder <WRyder505@gmail.com>
2025-06-03 15:13:07 -0700

    Project Setup

---
+++ test/Types.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Types', () => {
+	beforeEach(async () => {
+		const Contract = await ethers.getContractFactory('Types')
+		contract = await Contract.deploy()
+	})
+
+	describe('Example 1', () => {
+		it('demonstrates boolean values', async () => {
+
+		})
+	})
+})
