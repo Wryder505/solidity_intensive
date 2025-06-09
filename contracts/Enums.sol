ed9f9a13a84d646a811d4e1a273675be4370057a
W. Ryder <WRyder505@gmail.com>
2025-06-09 10:39:33 -0700

    Enums

---
+++ contracts/Enums.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// Enums
+contract Enums1 {
+
+	// Todo: 0
+	// InProgress: 1
+	// Done: 2
+	enum Status {
+		Todo,
+		InProgress,
+		Done
+	}
+
+	// Defaults to first item, e.g., "Todo"
+	Status public status;
+
+	function get() public view returns (Status) {
+		return status;
+	}
+
+	// Set status manually
+	function set(Status _status) public {
+		status = _status;
+	}
+
+	// Mark as done
+	function complete() public {
+		status = Status.Done;
+	}
+
+	// Resets to default value, i.e., Todo or 0
+	function reset() public {
+		delete status;
+	}
+}
